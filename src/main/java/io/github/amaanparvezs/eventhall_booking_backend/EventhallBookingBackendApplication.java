0ce8b0866c8193a8dcef92da9a577f0066cab023
Amaan Parvez <amaanparvez.cs@gmail.com>
2025-12-19 18:57:43 +0530

    first commit

---
+++ src/main/java/io/github/amaanparvezs/eventhall_booking_backend/EventhallBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.amaanparvezs.eventhall_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhallBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhallBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/amaanparvezs/eventhall_booking_backend/EventhallBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.amaanparvezs.eventhall_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhallBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
